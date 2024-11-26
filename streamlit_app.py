9a57a0020b9111416767668fbd88a6743c3b9f2f
RAFIIF <rafiiffadlurrohman99@gmail.com>
2024-11-26 22:39:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,46 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Inisialisasi data stok barang
+if 'data' not in st.session_state:
+    st.session_state.data = pd.DataFrame({
+        'Nama Barang': ['Barang A', 'Barang B', 'Barang C'],
+        'Stok': [10, 20, 30]
+    })
+
+# Fungsi untuk menambah stok
+def tambah_stok(nama_barang, jumlah):
+    st.session_state.data.loc[st.session_state.data['Nama Barang'] == nama_barang, 'Stok'] += jumlah
+
+# Fungsi untuk mengurangi stok
+def kurangi_stok(nama_barang, jumlah):
+    st.session_state.data.loc[st.session_state.data['Nama Barang'] == nama_barang, 'Stok'] -= jumlah
+
+st.title('Manajemen Stok Barang')
+
+# Form untuk menambah stok
+st.header('Tambah Stok')
+with st.form('tambah_stok_form'):
+    nama_barang_tambah = st.selectbox('Pilih Barang', st.session_state.data['Nama Barang'])
+    jumlah_tambah = st.number_input('Jumlah', min_value=1, step=1)
+    submit_tambah = st.form_submit_button('Tambah Stok')
+    if submit_tambah:
+        tambah_stok(nama_barang_tambah, jumlah_tambah)
+        st.success(f'Stok {nama_barang_tambah} berhasil ditambah sebanyak {jumlah_tambah}.')
+
+# Form untuk mengurangi stok
+st.header('Kurangi Stok')
+with st.form('kurangi_stok_form'):
+    nama_barang_kurangi = st.selectbox('Pilih Barang', st.session_state.data['Nama Barang'])
+    jumlah_kurangi = st.number_input('Jumlah', min_value=1, step=1)
+    submit_kurangi = st.form_submit_button('Kurangi Stok')
+    if submit_kurangi:
+        if st.session_state.data.loc[st.session_state.data['Nama Barang'] == nama_barang_kurangi, 'Stok'].values[0] >= jumlah_kurangi:
+            kurangi_stok(nama_barang_kurangi, jumlah_kurangi)
+            st.success(f'Stok {nama_barang_kurangi} berhasil dikurangi sebanyak {jumlah_kurangi}.')
+        else:
+            st.error('Jumlah pengurangan melebihi stok yang tersedia.')
+
+# Tampilkan data stok barang
+st.header('Daftar Stok Barang')
+st.table(st.session_state.data)
