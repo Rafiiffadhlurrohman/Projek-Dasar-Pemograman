a28cb599cbc1be29135491f5af7f20192545210c
RAFIIF <rafiiffadlurrohman99@gmail.com>
2024-11-26 23:01:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ def load_data():
         data = pd.read_csv('stok_sembako.csv')
     except FileNotFoundError:
         data = pd.DataFrame({
-            'Nama Barang': ['Beras', 'Gula', 'Minyak Goreng', 'Telur', 'Tepung', 'Garam', 'Kopi', 'Asem'],
+            'Nama Barang': ['Beras', 'Gula', 'Minyak Goreng', 'Telur', 'Tepung', 'Garam', 'Kopi', 'Teh'],
             'Stok': [200, 100, 300, 400, 500, 700, 800, 600]
         })
     return data
